0d49afc6605df45206f5537ac373950c38773554
Grace <gchen20@stuy.edu>
2020-11-23 17:55:27 -0500

    Tested and implemented add method

--- Tester.java
+++ Tester.java
@@ -1,8 +1,9 @@
 public class Tester{
   public static void main(String[]args) {
-    RealNumber num1 = new RealNumber(10.0000001);
-    RealNumber num2 = new RealNumber(10.0000003);
-    System.out.println(num1.equals(num2));
+    RealNumber num1 = new RealNumber(99.9);
+    RealNumber num2 = new RealNumber(-100);
+    // System.out.println(num1.equals(num2));
+    System.out.println(num1.add(num2));
 
   }
 }
